a78ebf43cc474e20fc9f7078ae2e99138475e008
etyoss <jamie.xue@gmail.com>
2013-06-26 21:44:39 -0400

    Added more param control for history command

--- goobo/main/record.py
+++ goobo/main/record.py
@@ -1,10 +1,8 @@
 """
 hint.py - Hint Module
 """
-import datetime
 import pytz
 from django.conf import settings
-from django.db import IntegrityError
 from main.module import command
 from main.models import Record
 
@@ -24,10 +22,24 @@ def record(goobo, reply_to, command_str):
                           settings.COMMAND_PREFIX, CMD_STR))
         return
     # query hint
-    keyword = command_str
-    records = Record.objects.filter(keyword=keyword)
+    cmd_parts = command_str.split()
+    keyword = cmd_parts[0]
+
+    number = 3
+    dt_format = "0"
+    if len(cmd_parts) > 1:
+        number = cmd_parts[1]
+    if len(cmd_parts) > 2:
+        dt_format = cmd_parts[2]
+
+    records = Record.objects.filter(keyword=keyword)\
+        .order_by("-date_created")[:number]
     for rec in records:
         local_dt = rec.date_created.astimezone(LOCAL_TZ)
-        say_str = u"{} ({}) {}".format(local_dt.strftime("%H:%M"),
+        if dt_format == "1":
+            local_dt_str = local_dt.strftime("%b %d %H:%M")
+        else:
+            local_dt_str = local_dt.strftime("%H:%M")
+        say_str = u"{} ({}) {}".format(local_dt_str,
                                        rec.sender, rec.message)
         goobo.say(reply_to, say_str, delay=0.2)
